6554bd39c909bd89a31969802adc443e839f6606
arachtivix <danielcswerner@gmail.com>
2025-06-12 15:32:43 +0000

    remaining demo code

--- src/potato_catalog/handler.clj
+++ src/potato_catalog/handler.clj
@@ -1,15 +1,34 @@
 (ns potato-catalog.handler
-  (:require [compojure.core :refer [defroutes GET]]
+  (:require [compojure.core :refer [defroutes GET POST]] ; add POST here
             [compojure.route :as route]
+            [potato-catalog.views :as views] ; add this require
             [ring.adapter.jetty :as jetty]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
-(defroutes app-routes
-  (GET "/" [] "Hello World")
+(defroutes app-routes ; replace the previous app-routes with this
+  (GET "/"
+    []
+    (views/home-page))
+  (GET "/add-location"
+    []
+    (views/add-location-page))
+  (POST "/add-location"
+    {params :params}
+    (views/add-location-results-page params))
+  (GET "/location/:loc-id"
+    [loc-id]
+    (views/location-page loc-id))
+  (GET "/all-locations"
+    []
+    (views/all-locations-page))
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
   (wrap-defaults #'app-routes site-defaults))
+
+(defn -main []
+  (jetty/run-jetty #'app {:port 3000}))
 
 (defn run-server []
   (jetty/run-jetty #'app {:port 3000}))
